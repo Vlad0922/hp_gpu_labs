7652c5ca4cef01e7c890a4a58cfcf1182a53d4e0
vlad0922 <vlad0922@yandex.ru>
2017-11-11 20:26:43 +0300

    [fix] little change in plot script

--- cuda_lab_1/plot.py
+++ cuda_lab_1/plot.py
@@ -7,5 +7,10 @@ data = pd.read_csv('results.csv', sep=';')
 
 plt.plot(data['size'], data['cpu'], label='cpu', color='blue')
 plt.plot(data['size'], data['gpu'], label='gpu', color='red')
+
+plt.xlabel('"A" matrix size')
+plt.ylabel('Time in ms')
+plt.title('Average cpu/gpu ratio: {}'.format( (data['cpu']/data['gpu']).mean() ))
+
 plt.legend()
 plt.show()
